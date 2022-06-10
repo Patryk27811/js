0f4b1908a1dcdb6e99d9dd3d0a3657df57d10e9a
Patryk27811 <patryk.dobrowolski24@gmail.com>
2022-06-11 00:13:52 +0200

    .

---
+++ pracadodatkowasnake/gra.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './waz.js'
+import { update as updateFood, draw as drawFood } from './jedzenie.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('plansza')
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm('Przegrałeś. Wcisnij ok by zgrac')) {
+      window.location = '/'
+    }
+    return
+  }
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
+}
+
+function draw() {
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
